76dccc012f9630ed4365b9abfbd4942aa6ca2df4
DESKTOP-NU36G62\WANG <wang8763@naver.com>
2024-12-20 23:10:55 +0900

    리사이클러 뷰 및 클릭 리스너 구현

--- app/src/main/java/com/example/control_calculator/Calculator_Adapter.kt
+++ app/src/main/java/com/example/control_calculator/Calculator_Adapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class Calculator_Adapter(val itemList: ArrayList<Calculator_Item>) :
@@ -12,11 +13,17 @@ class Calculator_Adapter(val itemList: ArrayList<Calculator_Item>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Calculator_ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.rycyclerview_calculator, parent, false)
-        return Calculator_ViewHolder(view)
+        return Calculator_ViewHolder(view).apply {
+            itemView.setOnClickListener {
+                val curPos : Int = adapterPosition
+                val calculator: Calculator_Item = itemList.get(curPos)
+                Toast.makeText(parent.context, "계산기 : ${calculator.name}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: Calculator_ViewHolder, position: Int) {
-        holder.calculator_image = itemList[position].imageview
+        holder.calculator_image.setImageResource(itemList[position].Drawable)
         holder.calculator_name.text = itemList[position].name
         holder.calculator_description.text = itemList[position].description
     }
@@ -29,5 +36,6 @@ class Calculator_Adapter(val itemList: ArrayList<Calculator_Item>) :
         var calculator_image = itemView.findViewById<ImageView>(R.id.calculator_picture)
         val calculator_name = itemView.findViewById<TextView>(R.id.calculator_Name)
         val calculator_description = itemView.findViewById<TextView>(R.id.calculator_Description)
+
     }
 }
